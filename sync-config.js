981b365d2ee9882c57e1c70c765b486a40d5cd82
seals-sherbet <seals-sherbet@users.noreply.github.com>
2026-05-12 11:53:46 -0500

    Configure Supabase sync

--- sync-config.js
+++ sync-config.js
@@ -1,5 +1,5 @@
 window.TASKS_SYNC_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
-  redirectUrl: ""
+  supabaseUrl: "https://rprqxkwygoulwebxgqac.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwcnF4a3d5Z291bHdlYnhncWFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MDIxOTYsImV4cCI6MjA5NDE3ODE5Nn0.ykETY9V74dhQpbVqIjMMvZwXyaM6IbFt4SSUXiWmdZc",
+  redirectUrl: "https://seals-sherbet.github.io/todo-app/"
 };
